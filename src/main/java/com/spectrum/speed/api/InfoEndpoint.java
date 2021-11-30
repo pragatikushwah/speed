3be2e618f8225b038aacf15c036e7dd12720c8d5
techsriman <tech.sriman.com>
2021-11-30 10:22:30 +0530

    added info endpoint

---
+++ src/main/java/com/spectrum/speed/api/InfoEndpoint.java
@@ -0,0 +1,13 @@
+package com.spectrum.speed.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoEndpoint {
+	@GetMapping(value = "/info.htm", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public String info() {
+		return "{\"instanceId\": \"" + this.hashCode() + "\"}";
+	}
+}
